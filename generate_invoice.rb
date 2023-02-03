362bd844d1c59412e07ab1010bcf343012409d03
PrathapPaapi <myselfprathap@gmail.com>
2023-02-03 18:10:11 +0530

    -create objects of every invoice to be generated and store it
    -created class for invoice generation
    
    -invoice generation and printing invoice are made separate

--- generate_invoice.rb
+++ generate_invoice.rb
@@ -1,28 +1,22 @@
 module GenerateInvoice
     def generate_invoice(required_entry)
 
+        new_invoice_id = invoice_constant + self.invoice_id_reference.to_s
+
+        self.invoice_id_reference += 1
+
         exit_time = Time.now
 
-        parked_duration = (exit_time - required_entry.entry_details.entry_time)
-
-        puts "****************************"
-        puts "\n"
-        puts "Generating invoice for #{required_entry.entry_details.license_plate}"
-        puts "\n"
-
-        puts "Invoice Number  : #{required_entry.invoice_ID}"
-
-        puts "License Plate   : #{required_entry.entry_details.license_plate}"
-
-        puts "Entry Time      : #{required_entry.entry_details.entry_time}"
-        
-        puts "Exit Time       : #{exit_time}"
-        
-        puts "Parked Duration : #{parked_duration} seconds"
+        parked_duration = (exit_time - required_entry.entry_time)
 
         parking_charges = get_parking_charges(parked_duration)
 
-        puts "Parking Charges : Rs. #{parking_charges}"
+        new_invoice = InvoiceDetails.new(required_entry.license_plate, new_invoice_id, 
+                                    required_entry.entry_time, exit_time, parked_duration, parking_charges)
+
+        invoices.push(new_invoice)
+
+        print_invoice(new_invoice)
 
     end
 
@@ -47,6 +41,25 @@ module GenerateInvoice
 
         parking_charges
             
+    end
+
+    def print_invoice(invoice)
+        puts "****************************"
+        puts "\n"
+        puts "Generating invoice for #{invoice.license_plate}"
+        puts "\n"
+
+        puts "Invoice Number  : #{invoice.invoice_ID}"
+
+        puts "Entry Time      : #{invoice.entry_time}"
+        
+        puts "Exit Time       : #{invoice.exit_time}"
+        
+        puts "Parked Duration : #{invoice.parked_duration} seconds"
+
+        
+
+        puts "Parking Charges : Rs. #{invoice.parking_charges}"
     end
 
 end
