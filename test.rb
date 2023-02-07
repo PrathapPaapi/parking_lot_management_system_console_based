19609a914dbf763c5aae5145d634b40290d88a1a
PrathapPaapi <111801659+PrathapPaapi@users.noreply.github.com>
2023-02-07 17:04:05 +0530

    Delete test.rb

--- test.rb
+++
@@ -1,8 +0,0 @@
-options = { 1 => "Add entry of the Car.", 2 => "Remove entry of the Car.", 
-    3 => "Find the invoice", 4 => "List of all Cars in parking lot.", 
-    5 => "List of all invoices.", 6 => "Exit."
-  }
-
-options.each do |opt, description|
-    puts "\n #{opt}. #{description}"
-end
